5a96b38b2e19d9eb7a6dcdd8e9e457e01d9043e7
Rafastaria <guitronleafar@gmail.com>
2016-07-23 20:40:46 -0500

    second commit

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
 from flask import Flask
+from flask import Flask, render_template, request
+from flask import Flask, render_template, json, request
+
 app = Flask(__name__)
 
 
@@ -11,6 +14,17 @@ def main():
 @app.route("/home")
 def Home():
 	return render_template('home.html')
+
+
+@app.route('/SignUp', methods=['POST'])
+def LogIn():
+
+#Crear variables que reciban los formularios
+	_Email = request.form['inputEmail']
+	_password = request.form['inputPassword']
+	_Latitud = request.form['inputLatitud']
+	_Longitud = request.form['inputLongitud']
+
 
 
 
